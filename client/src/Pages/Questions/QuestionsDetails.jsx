80d1bbf117bd64264246c9f79283e5db2580e58c
boynamebharath <97751974+boynamebharath@users.noreply.github.com>
2023-03-02 20:34:07 +0530

    Update QuestionsDetails.jsx

--- client/src/Pages/Questions/QuestionsDetails.jsx
+++ client/src/Pages/Questions/QuestionsDetails.jsx
@@ -71,7 +71,7 @@ const QuestionsDetails = () => {
     const dispatch = useDispatch()
     const User = useSelector((state) => (state.currentUserReducer))
     const location = useLocation()
-    const url = 'https://stack-overflow-app.render.com/'
+    const url = 'http://localhost:3000'
 
     const handlePostAns = (e, answerLength) =>{
         e.preventDefault()
